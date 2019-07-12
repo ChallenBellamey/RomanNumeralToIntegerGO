304dd2e03ed5afa7839a061e4c1deacb2bf9809f
Challen Bellamey <challenbellamey@hotmail.com>
2019-07-12 02:51:26 +0100

    Add /api route and getInfo func.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,6 +11,7 @@ import (
 
 func main() {
 	app := mux.NewRouter()
+	routes(app)
 	listen(app)
 }
 
@@ -17,4 +19,25 @@ func main() {
 
 func listen(router *mux.Router) {
 	http.ListenAndServe(":9090", router)
+}
+
+// routes
+
+func routes(router *mux.Router) {
+	router.HandleFunc("/api", getInfo).Methods("GET")
+}
+
+// api-controller
+
+func getInfo(writer http.ResponseWriter, request *http.Request) {
+	message := "Hello! Please attach a query to your get request to receive a Roman numerals conversion, i.e. " + request.Host + "/api/roman-numerals-to-integer?rn=CC"
+
+	type Response struct {
+		Message string `json:"message"`
+	}
+
+	response := Response{Message: message}
+
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(response)
 }
